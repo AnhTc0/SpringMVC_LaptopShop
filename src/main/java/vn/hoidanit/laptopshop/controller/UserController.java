dc99e78efd1bd7f396ba90e535f0af8d4e561c4e
TheAnh <anhdthe187386@fpt.edu.vn>
2025-08-19 12:23:04 +0700

    #46 Static file & first html

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -8,29 +8,29 @@ import org.springframework.web.bind.annotation.RestController;
 import vn.hoidanit.laptopshop.service.UserService;
 
 
-// @Controller
-// public class UserController {
-
-//     @RequestMapping("/")
-//     public String getHomepage(){
-//         return "homepage";
-//     }
-    
-// }
-
-@RestController
+@Controller
 public class UserController {
-    private final UserService userService;
-    
-
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
-
 
     @RequestMapping("/")
     public String getHomepage(){
-        return this.userService.handleHelloWorld();
+        return "home.html";
     }
-
+    
 }
+
+// @RestController
+// public class UserController {
+//     private final UserService userService;
+    
+
+//     public UserController(UserService userService) {
+//         this.userService = userService;
+//     }
+
+
+//     @RequestMapping("/")
+//     public String getHomepage(){
+//         return this.userService.handleHelloWorld();
+//     }
+
+// }
