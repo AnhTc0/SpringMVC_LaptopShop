d1a512cc9db508db3541560ca0de58fba2afea05
TheAnh <anhdthe187386@fpt.edu.vn>
2025-08-19 12:10:15 +0700

    Reformat and add folder, start using DI

---
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -0,0 +1,36 @@
+package vn.hoidanit.laptopshop.controller;
+
+import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import vn.hoidanit.laptopshop.service.UserService;
+
+
+// @Controller
+// public class UserController {
+
+//     @RequestMapping("/")
+//     public String getHomepage(){
+//         return "homepage";
+//     }
+    
+// }
+
+@RestController
+public class UserController {
+    private final UserService userService;
+    
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+
+    @RequestMapping("/")
+    public String getHomepage(){
+        return this.userService.handleHelloWorld();
+    }
+
+}
